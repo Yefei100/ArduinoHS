77ad5bd0902f7629111c13f1945efe25ec1c8132
Yefei100 <yefei100@outlook.com>
2013-12-06 10:12:47 -0500

    trying writing humidity data

--- Arduino SD/ReadWrite/ReadWrite.ino
+++ Arduino SD/ReadWrite/ReadWrite.ino
@@ -19,8 +19,9 @@
  */
  
 #include <SD.h>
+#include <SPI.h>
 
-File myFile;
+File myFile1;
 
 void setup()
 {
@@ -46,34 +47,34 @@ void setup()
   
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile1 = SD.open("HS_Humid.txt", FILE_WRITE);
   
   // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
+  if (myFile1) {
+    Serial.print("Writing to HS_Humid.txt...");
+    myFile1.println("Here is the Data for humidity");
 	// close the file:
-    myFile.close();
+    myFile1.close();
     Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening HS_Humid.txt");
   }
   
   // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
+  myFile1 = SD.open("HS_Humid.txt");
+  if (myFile1) {
+    Serial.println("HS_Humid.txt:");
     
     // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-    	Serial.write(myFile.read());
+    while (myFile1.available()) {
+    	Serial.write(myFile1.read());
     }
     // close the file:
-    myFile.close();
+    myFile1.close();
   } else {
   	// if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening HS_Humid.txt");
   }
 }
 
